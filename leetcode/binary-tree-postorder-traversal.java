24f9d4e60af041bf988c9526d68abb068511825c
riteshrana01 <riteshkumar01.work@gmail.com>
2025-10-27 11:48:30 +0530

    Update solution for binary-tree-postorder-traversal

--- leetcode/binary-tree-postorder-traversal.java
+++ leetcode/binary-tree-postorder-traversal.java
@@ -13,19 +13,47 @@
  *     }
  * }
  */
+
+// Go as far left as possible and push nodes on a stack.
+
+// When you can’t go left anymore:
+
+// If the top of the stack has an unvisited right child, move right.
+
+// Otherwise, process (visit) the node (add its value to result).
+
+// Continue until both left and right subtrees are processed.
+
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        helper(root, ans);
-        return ans;
-    }
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
 
-    void helper(TreeNode root, List<Integer> ans) {
-        if (root == null)
-            return;
+        TreeNode node = root;
 
-        helper(root.left, ans);
-        helper(root.right, ans);
-        ans.add(root.val);
+        while (node != null || !st.isEmpty()) {
+            // move left
+            while (node != null) {
+                st.push(node);
+                node = node.left;
+            }
+
+            // move right
+
+            if (st.peek().right != null) {
+                node = st.peek().right;
+            } else {
+                TreeNode temp = st.pop();
+                res.add(temp.val);
+
+                while (!st.isEmpty() && temp == st.peek().right) {
+                    temp = st.pop();
+                    res.add(temp.val);
+                }
+            }
+        }
+
+        return res;
     }
 }
