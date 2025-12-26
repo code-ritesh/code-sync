1ad23329925afc4125d8768272e7b4e9171d9302
riteshrana01 <riteshkumar01.work@gmail.com>
2025-12-26 21:59:56 +0530

    Update solution for largest-number

--- leetcode/largest-number.py
+++ leetcode/largest-number.py
@@ -14,7 +14,8 @@ class Solution {
         Arrays.sort(arr , (a,b) -> (b+a).compareTo(a+b));
 
 
-        if(arr[0] == "0") return "0";
+        if (arr[0].equals("0")) return "0";
+
 
 
         StringBuilder sb = new StringBuilder();
