e0f3ca3c35a8551ec6eae7113d31b8f2c7e238db
riteshrana01 <riteshkumar01.work@gmail.com>
2026-01-01 11:46:20 +0530

    Add solution for target-sum

---
+++ leetcode/target-sum.py
@@ -0,0 +1,38 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        // same as partitions with given difference on gfg
+
+        int n = nums.length;
+        int range = 0;
+        // target == diff
+
+        for(int x : nums ) range += x;
+
+        int s1 = (range + target ) / 2;
+
+        if(range < target || (target + range) % 2 == 1  ) return 0;
+
+        return helper(nums , s1);
+    }
+
+
+    int helper(int[] nums , int s1){
+        int n = nums.length;
+
+        int[][] dp = new int[n+1][s1+1];
+
+        for(int i = 0; i <= n ; i++) dp[i][0] = 1;
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 0 ; j <= s1 ; j++){
+                if(nums[i-1] <= j ){
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                }
+
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp[n][s1];
+    }
+}
