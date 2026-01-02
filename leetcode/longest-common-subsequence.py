96d10a7235b36a178f2db274d01074c4baa68be4
riteshrana01 <riteshkumar01.work@gmail.com>
2026-01-02 19:39:42 +0530

    Update solution for longest-common-subsequence

--- leetcode/longest-common-subsequence.py
+++ leetcode/longest-common-subsequence.py
@@ -1,22 +1,29 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
+    public int longestCommonSubsequence(String a, String b) {
+        int m = a.length();
+        int n = b.length();
 
-        int dp[][] = new int[m+1][n+1];
+        int dp[][] = new int[m + 1][n + 1];
 
-        for(int i = 1 ; i <= m ; i++){
-            for(int j = 1 ; j <= n ; j++){
-                if(text1.charAt(i-1) == text2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-
-                else dp[i][j] = Math.max(dp[i][j-1] , dp[i-1][j]);
-            }
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
 
-        return dp[m][n];
+        return helper(a, b, m, n, dp);
     }
 
-    
+    int helper(String a, String b, int m, int n, int[][] dp) {
+        if (m == 0 || n == 0)
+            return 0;
+
+        if (dp[m][n] != -1)
+            return dp[m][n];
+
+        if (a.charAt(m - 1) == b.charAt(n - 1)) {
+            return  dp[m][n] = 1 + helper(a, b, m - 1, n - 1 , dp);
+        }
+
+        else
+            return dp[m][n] = Math.max(helper(a, b, m - 1, n,dp), helper(a, b, m, n - 1,dp));
+    }
 }
