421fafee00da03b0053d36f4cba4a125a5eba78d
riteshrana01 <riteshkumar01.work@gmail.com>
2026-01-05 23:45:02 +0530

    Update solution for house-robber

--- leetcode/house-robber.py
+++ leetcode/house-robber.py
@@ -1,13 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-    if (nums.length == 0) return 0;
-    int[] memo = new int[nums.length + 1];
-    memo[0] = 0;
-    memo[1] = nums[0];
-    for (int i = 1; i < nums.length; i++) {
-        int val = nums[i];
-        memo[i+1] = Math.max(memo[i], memo[i-1] + val);
+        int n = nums.length;
+
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = Math.max(nums[i-1] + dp[i-2], dp[i-1]);
+        }
+
+        return dp[n];
     }
-    return memo[nums.length];
-}
 }
