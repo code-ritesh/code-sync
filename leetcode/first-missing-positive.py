0ad42ea7c9628161b64f4620f93c2812cd7aab6f
riteshrana01 <riteshkumar01.work@gmail.com>
2026-01-11 23:25:24 +0530

    Add solution for first-missing-positive

---
+++ leetcode/first-missing-positive.py
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        boolean[] help = new boolean[n+1];
+
+        for(int i = 0 ; i < n  ; i++){
+            int x = nums[i];
+
+            if( x >= 1 && x <= n ) help[x] = true;
+        }
+
+
+        for(int i = 1 ; i < n ; i++){
+            if( help[i] == false ) return i;
+        } 
+
+
+        return n;
+    }
+}
