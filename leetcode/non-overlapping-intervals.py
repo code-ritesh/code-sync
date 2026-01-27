c5e17c7e966e3dd5f36a42e4033afa3440556e18
riteshrana01 <riteshkumar01.work@gmail.com>
2026-01-27 22:03:13 +0530

    Add solution for non-overlapping-intervals

---
+++ leetcode/non-overlapping-intervals.py
@@ -0,0 +1,20 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+
+        int removed = 0;
+        int lastfinish = intervals[0][1];
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] < lastfinish) {
+                removed++;
+            } else {
+                lastfinish = intervals[i][1];
+            }
+        }
+
+        return removed;
+    }
+}
