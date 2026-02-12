fd6d75193c6d2dc75d05825e56356943d0cf2c32
ritesh <riteshkumar01.work@gmail.com>
2026-02-12 10:23:55 +0530

    Add solution for longest-consecutive-sequence

---
+++ leetcode/longest-consecutive-sequence.py
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        int curr = 1;
+        int max = 0;
+        int n = nums.length;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                curr++;
+            }
+
+            else if (nums[i - 1] == nums[i])
+                continue;
+
+            else {
+                curr = 1;
+            }
+
+            max = Math.max(curr, max);
+        }
+
+        return Math.max(curr, max);
+    }
+}
