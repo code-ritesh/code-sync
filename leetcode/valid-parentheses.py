6d0df9cec37cd8c4d4dc59117646bef5c69e7f76
ritesh <riteshkumar01.work@gmail.com>
2026-02-13 01:41:15 +0530

    Add solution for valid-parentheses

---
+++ leetcode/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+            if (x == '(')  st.add(')');
+            else if (x == '{') st.add('}');
+            else if (x == '[') st.add(']');
+
+            else if (st.isEmpty() || st.peek() != x)
+                return false;
+
+            else st.pop();
+        }
+
+        return true;
+    }
+}
