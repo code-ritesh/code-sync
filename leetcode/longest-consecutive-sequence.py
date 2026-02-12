80c2021a3457091cd52bd94801d7aed4a6e50ad4
ritesh <riteshkumar01.work@gmail.com>
2026-02-12 10:24:27 +0530

    Update solution for longest-consecutive-sequence

--- leetcode/longest-consecutive-sequence.py
+++ leetcode/longest-consecutive-sequence.py
@@ -5,6 +5,8 @@ class Solution {
         int max = 0;
         int n = nums.length;
 
+        if(n == 0 ) return 0;
+
         for (int i = 1; i < n; i++) {
             if (nums[i] == nums[i - 1] + 1) {
                 curr++;
