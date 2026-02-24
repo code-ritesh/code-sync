ce7f3cd4f2ffa8cc883d349db4d39eb14aa34899
ritesh <riteshkumar01.work@gmail.com>
2026-02-24 22:38:11 +0530

    Add solution for search-a-2d-matrix

---
+++ leetcode/search-a-2d-matrix.py
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix[0].length;
+        int m = matrix.length;
+
+        int s = 0 ;
+        int e = (m*n)-1;
+
+        while(s <= e ){
+            int mid = s + (e-s)/2;
+            int row = mid/n;
+            int col = mid %n;
+
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target ) s = mid+1;
+            else e = mid-1;
+        }
+
+        return false;
+    }
+}
