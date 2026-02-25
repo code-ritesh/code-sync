a3f80e1c8cbbb4f6082e4f63daee146411dd6e9e
ritesh <riteshkumar01.work@gmail.com>
2026-02-25 15:09:04 +0530

    Add solution for spiral-matrix-ii

---
+++ leetcode/spiral-matrix-ii.py
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int left = 0;
+        int right = n-1;
+        int top = 0;
+        int bottom = n-1;
+
+        int[][] ans = new int[n][n];
+        int val = 1;
+
+        while(left <= right && top <= bottom ){
+
+            for(int i = left ; i <= right ; i++) ans[top][i] = val++;
+            top++;
+
+            for(int i = top ; i <= bottom ; i++ ) ans[i][right] = val++;
+            right--;
+
+            if(top <= bottom ){
+                for(int i = right ; i >= left ; i--) ans[bottom][i] = val++;
+                bottom--;
+            }
+
+            if(left <= right){
+                for(int i = bottom ; i >= top ; i--) ans[i][left] = val++;
+                left++;
+            }
+        } 
+
+        return ans;
+    }
+}
