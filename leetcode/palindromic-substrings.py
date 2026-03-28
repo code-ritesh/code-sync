2832138d97c8c8586ab61c6868c28e4da78bdf16
ritesh <riteshkumar01.work@gmail.com>
2026-03-28 08:13:46 +0530

    Add solution for palindromic-substrings

---
+++ leetcode/palindromic-substrings.py
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            count += helper(s, i, i); // odd length
+            count += helper(s, i, i + 1); //even length
+        }
+
+        return count;
+    }
+
+    int helper(String s, int i, int j) {
+
+        int count = 0;
+
+        while (j < s.length() && i >= 0 && s.charAt(i) == s.charAt(j)) {
+
+            System.out.println(s.substring(i, j + 1));
+            count++;
+            i--;
+            j++;
+        }
+
+        return count;
+    }
+}
