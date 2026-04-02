d7352d346c70cf69d09aa8485432c61d4c949e85
ritesh <riteshkumar01.work@gmail.com>
2026-04-02 10:32:44 +0530

    Add solution for maximum-average-subarray-i

---
+++ leetcode/maximum-average-subarray-i.py
@@ -0,0 +1,22 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        int max = 0;
+
+        for(int i= 0 ; i < k ; i++){
+            sum += nums[i];
+        }
+
+        max = sum;
+
+        for(int i = k ; i < nums.length ; i++){
+            sum -= nums[i-k];
+            sum += nums[i];
+
+            max = Math.max(sum,max);
+        }
+
+
+        return (double)max/k;
+    }
+}
