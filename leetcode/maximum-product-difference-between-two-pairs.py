ba3bee8ea5c512353d28a81c4131f54b900a3968
ritesh <riteshkumar01.work@gmail.com>
2026-04-09 20:01:46 +0530

    Add solution for maximum-product-difference-between-two-pairs

---
+++ leetcode/maximum-product-difference-between-two-pairs.py
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int a = nums[n-1];
+        int b = nums[n-2];
+
+        int c = nums[0];
+        int d = nums[1];
+
+        return (a*b) - (c*d);
+    }
+}
