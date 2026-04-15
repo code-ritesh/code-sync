85126d57cbdb226f53a40a2b6ed84c45e8d8f24a
ritesh <riteshkumar01.work@gmail.com>
2026-04-15 14:52:03 +0530

    Update solution for add-two-numbers

--- leetcode/add-two-numbers.java
+++ leetcode/add-two-numbers.java
@@ -11,31 +11,27 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
+        ListNode curr= dummy;
 
         int carry = 0;
 
-        while(l1 != null || l2 != null || carry != 0 ){
-
+        while(l1 != null || l2 != null || carry != 0){
             int sum = carry;
 
-            if( l1 != null ){
+            if(l1 != null ){
                 sum += l1.val;
                 l1 = l1.next;
             }
 
-            if(l2 != null ){
+            if( l2 != null ){
                 sum += l2.val;
                 l2 = l2.next;
             }
 
-            
 
-            carry = sum/10;
             int ln = sum % 10;
-
+            carry = sum / 10;
             ListNode temp = new ListNode(ln);
-
             curr.next = temp;
             curr = curr.next;
         }
