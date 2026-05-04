e2dee7fd1c36ee896053c67dda15f48928aab994
ritesh <riteshkumar01.work@gmail.com>
2026-05-04 09:39:05 +0530

    Update solution for product-of-array-except-self

--- leetcode/product-of-array-except-self.py
+++ leetcode/product-of-array-except-self.py
@@ -1,26 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
+        int[] p = new int[n];
+        int[] s = new int[n];
 
-        int[] pref  = new int[n];
-        int[] suff = new int[n];
+        p[0] = 1;
+        s[n - 1] = 1;
 
-        Arrays.fill(pref,1);
-        Arrays.fill(suff,1);
-
-        pref[0] = 1;
-        suff[n-1] = 1;
-
-        for(int i = 1 ; i < n ; i++){
-            pref[i] = pref[i-1] * nums[i-1];
+        for (int i = 1; i < n; i++) {
+            p[i] = p[i - 1] * nums[i-1];
         }
 
-        for(int i = n-2 ; i >= 0 ; i--){
-            suff[i] = suff[i+1] * nums[i+1];
+        for (int i = n - 2; i >= 0; i--) {
+            s[i] = s[i + 1] * nums[i+1];
         }
 
-        for(int i = 0 ; i < n ; i++) pref[i] *= suff[i];
 
-        return pref;
+        for (int i = 0; i < n; i++) {
+            p[i] *= s[i];
+        }
+
+        return p;
     }
+
 }
