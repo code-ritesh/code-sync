d6d5de11ffb2e34641821ce2f0f67e1489557f80
ritesh <riteshkumar01.work@gmail.com>
2026-05-06 09:34:34 +0530

    Update solution for house-robber

--- leetcode/house-robber.py
+++ leetcode/house-robber.py
@@ -1,21 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
+        int[] dp = new int[n];
 
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
+        if(n == 0 ) return 0;
+        if(n == 1 ) return nums[0];
 
-        int[] dp = new int[n+1];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
 
-        dp[0] = 0;
-        dp[1] = nums[0];
+        for(int i = 2 ; i < n ; i++){
+            int a = dp[i-1];
+            int b = dp[i-2] + nums[i];
 
-        for(int i = 2; i <= n; i++){
-            dp[i] = Math.max(nums[i-1] + dp[i-2], // rob the house
-             dp[i-1] // dont rob the house 
-             );
+            dp[i] = Math.max(a,b);
         }
 
-        return dp[n];
+        return dp[n-1];
     }
 }
